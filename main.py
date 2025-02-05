fac9f68ff29951280ca4f75067b893c4f3a6c050
Archer Wang <3762193+archer-trek@users.noreply.github.com>
2025-02-05 17:05:31 +0800

    add log

--- main.py
+++ main.py
@@ -22,6 +22,24 @@ def build_parser():
 
 
 if __name__ == '__main__':
+    from logging.config import dictConfig
+
+    dictConfig({
+        'version': 1,
+        'formatters': {'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+        'handlers': {'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        }},
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
+
     parser = build_parser()
     args = parser.parse_args()
 
